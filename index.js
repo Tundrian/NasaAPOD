9f60c79fd644c2985473c4f25790cf89c76da042
Tundrian <antoniosansotta@gmail.com>
2022-04-12 16:14:23 -0400

    added video playback capability

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=8efwoPdq02GYBdDZqFNBDDfdJWqzc
         document.querySelector('.title').innerText = data.title
         //document.querySelector('.url').src = data.url
         document.querySelector('.date').innerText = data.date
-        document.querySelector('.copyright').innerText = data.copyright
-        document.querySelector('.hdurl').src = data.hdurl
-        // document.querySelector('.mediaType').innerText = data.media_type
+        document.querySelector('.copyright').innerText = data.copyright || ''
+        if(data.media_type === 'image'){
+            document.querySelector('.hdurl').src = data.hdurl
+        } else if(data.media_type === 'video'){
+
+            document.querySelector('.video-container').toggleAttribute('hidden')
+            document.querySelector('.video').src = data.hdurl
+        }
+        
+        document.querySelector('.mediaType').innerText = data.media_type
         document.querySelector('.explanation').innerText = data.explanation
     })
     .catch(err => console.log(`Error: ${err}`))
